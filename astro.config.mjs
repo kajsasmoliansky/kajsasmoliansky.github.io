a542b01778ae9e9572526ef20847fae488164d45
David Brockman <13208+dbrockman@users.noreply.github.com>
2026-01-16 18:52:19 +0100

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dbrockman.github.io',
+  site: 'https://kajsasmoliansky.github.io',
   build: {
     assets: '_assets'
   },
